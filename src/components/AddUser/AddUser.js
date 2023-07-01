2cfed0267cc374104718ebf70f66fabd58f1da36
Elio Borges <92939227+Xremix34@users.noreply.github.com>
2023-07-01 18:27:29 +0000

    Update 17 files

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,7 +1,79 @@
-import classes from './AddUser.module.css';
+import React, { useState } from "react";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button/Button";
+import Card from "../UI/Card/Card";
+import ErrorModal from "../UI/ErrorModal/ErrorModal";
 
-const AddUser = () =>{
-  return();
-}
+const AddUser = (props) => {
+  const [inputAddUser, setInputAddUser] = useState("");
+  const [inputAddAge, setInputAddAge] = useState("");
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    setInputAddUser(event.target.value);
+  };
+
+  const addAgeHandler = (event) => {
+    setInputAddAge(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (inputAddUser.trim().length === 0 || inputAddAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+      return;
+    }
+    if (+inputAddAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
+      return;
+    }
+    props.onAddUser(inputAddUser, inputAddAge);
+    setInputAddUser("");
+    setInputAddAge("");
+  };
+
+  const errorHandle = () => {
+    setError(null);
+  };
+
+  return (
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandle}
+        />
+      )}
+
+      <Card className={classes.input}>
+        <form onSubmit={submitHandler}>
+          <label htmlFor="name-user">Name</label>
+
+          <input
+            onChange={addUserHandler}
+            value={inputAddUser}
+            type="text"
+            id="name-user"
+          />
+          <label htmlFor="year-user">Age</label>
+          <input
+            onChange={addAgeHandler}
+            value={inputAddAge}
+            type="number"
+            id="yea-user"
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
 
 export default AddUser;
--- src/components/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import classes from './Button.module.css';
-
-const Button = () =>{
-  return();
-};
-
-export default Button;
